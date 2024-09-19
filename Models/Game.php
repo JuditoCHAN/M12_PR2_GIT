b26d8506ed7c98f64d8c27a0bdf2229c9c318e9e
judith <judith.bello.torralbo@gmail.com>
2024-09-19 16:18:47 +0200

    Cambio ArrayList por ArrayObject y hago un import de ArrayObject

--- Models/Game.php
+++ Models/Game.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-use Nette\Utils\ArrayList;
+//use Nette\Utils\ArrayList;
+use ArrayObject;
 
 /**
  * Repr
@@ -50,7 +51,7 @@ class Game
   /**
    * Get team #1 vs. Team #2
    *
-   * @return  ArrayList
+   * @return  ArrayObject
    */ 
   public function getTeams()
   {
@@ -60,11 +61,11 @@ class Game
   /**
    * Set team #1 vs. Team #2
    *
-   * @param  ArrayList  $teams  Team #1 vs. Team #2
+   * @param  ArrayObject  $teams  Team #1 vs. Team #2
    *
    * @return  self
    */ 
-  public function setTeams(ArrayList $teams)
+  public function setTeams(ArrayObject $teams)
   {
     $this->teams = $teams;
     
